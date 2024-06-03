f518aebba5f665296a09cd939e7d9ccf3a7ce23a
Veritaaaas <rodneyjamesmmaisog@iskolarngbayan.pup.edu.ph>
2024-06-03 20:55:51 +0800

    add registration endpoint to backend app

--- backend/app.py
+++ backend/app.py
@@ -2,8 +2,8 @@ from flask import Flask, session, redirect, url_for, escape, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def
+@app.route('/register', methods=['POST'])
+def 
 
 if __name__ == '__main__':
     app.run(debug=True)
