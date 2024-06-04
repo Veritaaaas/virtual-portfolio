d8da58c16ef7fa896cbb2ce13389a7f14e4511a8
Veritaaaas <rodneyjamesmmaisog@iskolarngbayan.pup.edu.ph>
2024-06-04 23:23:44 +0800

    add mysql support

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,29 @@
-from flask import Flask, session, redirect, url_for, escape, request
+from flask import Flask
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-@app.route('/register', methods=['POST'])
-def 
+# MySQL configurations
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'mysql'
+app.config['MYSQL_DB'] = 'financedb'
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_PORT'] = 3306
+
+mysql = MySQL(app)
+
+@app.route('/')
+def index():
+    # create a connection
+    cur = mysql.connection.cursor()
+    # execute a query
+    cur.execute("INSERT INTO users (username, first_name, last_name, password, cash) VALUES ('John', 'Doe', 'Smith', 'defaultPassword', 1000)")
+    # commit the transaction
+    mysql.connection.commit()
+    # close the cursor
+    cur.close()
+
+    return 'Data inserted.'  # return a success message
 
 if __name__ == '__main__':
     app.run(debug=True)
