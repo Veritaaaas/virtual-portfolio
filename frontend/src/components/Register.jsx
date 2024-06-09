9806409ec102a80e82f1dc71f870f76dc11b29e3
Veritaaaas <rodneyjamesmmaisog@iskolarngbayan.pup.edu.ph>
2024-06-09 14:12:42 +0800

    modify the json content in register component

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -10,22 +10,23 @@ function Register() {
 
     const handleSubmit = async (event) => {
         event.preventDefault();
-
-        const response = await fetch('http://localhost:5000/register', {
+    
+        const fullName = `${firstName} ${lastName}`;
+    
+        const response = await fetch('http://127.0.0.1:5000/register', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify({
-                firstName,
-                lastName,
+                name: fullName,
                 username,
                 password,
             }),
         });
-
+    
         const data = await response.json();
-
+    
         console.log(data);
     };
 
