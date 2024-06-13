704e7b923df1f2cfa129ddef9e5d1e22e5392503
Veritaaaas <rodneyjamesmmaisog@iskolarngbayan.pup.edu.ph>
2024-06-13 20:58:22 +0800

    Add Navbar component

---
+++ frontend/src/components/Dashboard.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./Navbar"
+
+function Dashboard() {
+    return (
+        <div className="w-full h-full">
+            <Navbar />
+        </div>
+    )
+}
+
+export default Dashboard
