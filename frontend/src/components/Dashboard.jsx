53dfbc8c4a567d87516caec984486ca9ee9fc90a
Veritaaaas <rodneyjamesmmaisog@iskolarngbayan.pup.edu.ph>
2024-06-16 19:59:08 +0800

    add the component for viewing the stocks portfolio

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -16,15 +16,35 @@ function Dashboard() {
                         <h1 className="font-bold text-[#453DE0] text-[40px] ml-5">10 000 USD</h1>
                     </div>
                 </div>
-                <div className="bg-white w-[300px] py-4 flex flex-col items-center gap-4">
+                <div className="bg-white w-[300px] py-4 flex flex-col items-center gap-4 rounded-xl">
                     <h1 className="font-bold text-[#453DE0] text-[35px]">Deposit</h1>
                     <input type="text" className="rounded-lg w-[200px] h-[40px] bg-[#EBEEFB]" />
                     <button className="bg-[#453DE0] text-white w-[150px] h-[40px] rounded-lg mt-4">Deposit Now</button>
                 </div>
-                <div className="bg-white w-[300px] py-4 flex flex-col items-center gap-4">
+                <div className="bg-white w-[300px] py-4 flex flex-col items-center gap-4 rounded-xl">
                     <h1 className="font-bold text-[#453DE0] text-[35px]">Withdraw</h1>
                     <input type="text" className="rounded-lg w-[200px] h-[40px] bg-[#EBEEFB]" />
                     <button className="bg-[#453DE0] text-white w-[150px] h-[40px] rounded-lg mt-4">Withdraw Now</button>
+                </div>
+            </div>
+            <div className="px-16">
+                <div className="bg-white">
+                    <table className="w-full">
+                        <tr className="text-center">
+                            <th>Symbol</th>
+                            <th>Company Name</th>
+                            <th>Shares</th>
+                            <th>Price</th>
+                            <th>Total</th>
+                        </tr>
+                        <tr className="text-center">
+                            <td>GOOGL</td>
+                            <td>Google</td>
+                            <td>10</td>
+                            <td>10</td>
+                            <td>100</td>
+                        </tr>
+                    </table>
                 </div>
             </div>
         </div>
