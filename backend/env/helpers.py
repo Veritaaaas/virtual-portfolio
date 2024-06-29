0062f92f508f164669876a8e42b3e056a2b0eb5e
Veritaaaas <rodneyjamesmmaisog@iskolarngbayan.pup.edu.ph>
2024-06-29 13:32:10 +0800

    Add function for querying stock symbols

--- backend/env/helpers.py
+++ backend/env/helpers.py
@@ -15,6 +15,42 @@ def lookup(symbol):
         "company_name": company_name,
         "current_price": current_price
     }
+    
+def query(symbol):
+    # Get the stock data
+    stock = yf.Ticker(symbol)
+
+    # Get the company name
+    company_name = stock.info['shortName']
+    
+    # Get the company symbol
+    company_symbol = stock.info['symbol']
+
+    # Get the EPS ratio
+    eps_ratio = stock.info['trailingEps']
+
+    # Get the PE Ratio
+    pe_ratio = stock.info['trailingPE']
+
+    # Get the Revenue Growth
+    revenue_growth = stock.info['revenueGrowth']
+
+    # Get the Net Income
+    net_income = stock.info['netIncomeToCommon']
+
+    # Get the current stock price
+    current_price = stock.history(period="1d").tail(1)['Close'].iloc[0]
+
+    return {
+        "name": company_name,
+        "symbol": company_symbol,
+        "eps": eps_ratio,
+        "pe_ratio": pe_ratio,
+        "revenue_growth": revenue_growth,
+        "net_income": net_income,
+        "price": current_price
+    }
+    
 
 def USD(value):
     return "${:,.2f}".format(value)
